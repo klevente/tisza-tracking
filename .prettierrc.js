136f2c14015fe15f2ee8d082db03d3fc29cf000b
Levente Krizsan <lewalevi@gmail.com>
2024-08-09 14:09:24 +0200

    chore(project): scaffolding

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: false,
+  trailingComma: "all",
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "never",
+};
+
+export default config;

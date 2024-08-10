41705a2c87fa824f90a8361131ded4bc0b94a4cc
Levente Krizsan <lewalevi@gmail.com>
2024-08-10 16:12:51 +0200

    ci(project): add CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -74,6 +74,20 @@ module.exports = {
         "plugin:import/recommended",
         "plugin:import/typescript",
       ],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            args: "all",
+            argsIgnorePattern: "^_",
+            caughtErrors: "all",
+            caughtErrorsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            ignoreRestSiblings: true,
+          },
+        ],
+      },
     },
 
     // Node
